3a2ae312bbd1b49d081e193c44cb080ab6fc8e36
linlx <linlx@example.com>
2020-04-28 15:19:29 +0800

    首页图片链接完成

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,8 +10,6 @@ Page({
     catesList: [],
     //楼层数据
     floorList: [],
-    //楼层数据取出的query值
-    queryUrl: []
   },
   //页面开始加载，就会触发
   onLoad(options) {
@@ -48,34 +46,30 @@ Page({
         this.setData({
           catesList: result
         })
-      })  
+      })
   },
+
   //获取楼层数据
   getFloorList() {
-    var queryUrl=new Array();
-    var navigatorUrl;
+    let navigatorUrl;
     request({
         url: "/home/floordata"
       })
       .then(result => {
-        this.setData({
-          floorList: result
-        });
         let product_list = result.map(v => v.product_list);
         product_list.forEach(item => {
           item.forEach(item => {
             let navigator_url = item.navigator_url;
-            let navigatorUrlOne = (navigator_url).substring(0, 17);
             let query = navigator_url.substring(17);
-            navigatorUrl = navigatorUrlOne + "/goods_list" + query;
-            queryUrl.push(navigatorUrl);
-          })        
+            navigatorUrl = "/pages/goods_list/goods_list" + query;
+            item.navigator_url = navigatorUrl;
+          })
         });
+        //将楼层数据放入appData中
         this.setData({
-          queryUrl
-        })
+          floorList: result
+        });
 
       })
-
   },
 })
